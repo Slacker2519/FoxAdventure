6a25528d8c4686fbcc02327629ede29f05fb33f9
Slacker <tienduongeng2001@gmail.com>
2022-07-27 00:46:30 +0700

    Finish design level 1, tried make loading scene function and failed

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var ItemWithTag = collision.gameObject.tag;
+
+        if (ItemWithTag == "Goal")
+        {
+            LoadNextLevel();
+        }
+        else if (ItemWithTag == "Enemy")
+        {
+            ReloadLevel();
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
