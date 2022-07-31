2cb8cfb61dac86fb6d2be4ce93f9249a078e35aa
Slacker <tienduongeng2001@gmail.com>
2022-07-31 14:47:15 +0700

    Finish level1 and 2 added teleportation script starting make level 3

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    Vector2 basePosition;
+    [SerializeField] Vector2 baseRange;
+    float baseRangeDistance;
+
+    public GameObject player;
+    Vector2 playerPosition;
+    float playerDistance;
+
+    bool canTeleport = false;
+
+    void Awake()
+    {
+        basePosition = this.transform.position;
+        baseRangeDistance = Vector2.Distance(basePosition, baseRange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Teleportable();
+        if (Input.GetKeyDown(KeyCode.T) && canTeleport == true)
+        {
+            player.transform.position = basePosition;
+        }
+
+        Debug.Log("base range: " + baseRangeDistance);
+        Debug.Log("player distance" + playerDistance);
+    }
+
+    void Teleportable()
+    {
+        playerPosition = player.transform.position;
+        playerDistance = Vector2.Distance(playerPosition, basePosition);
+
+        if (baseRangeDistance >= playerDistance)
+        {
+            canTeleport = true;
+        }
+        else if (baseRangeDistance < playerDistance)
+        {
+            canTeleport = false;
+        }
+    }
+}
